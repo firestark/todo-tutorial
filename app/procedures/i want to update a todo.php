4627c2c7f6ad9c64f4df4890195ea107454bbf23
Aron Wouters <lionar@mail.com>
2019-07-09 12:37:20 +0200

    Corrected update business rule

--- app/procedures/i want to update a todo.php
+++ app/procedures/i want to update a todo.php
@@ -7,6 +7,9 @@ function ( todo $todo, todo\manager $manager )
     if ( ! $manager->has ( $todo ) )
         return [ 2001, [ ] ];
 
+    if ( $manager->hasTodoWithDescription ( $todo->description ) )
+        return [ 2000, [ ] ];
+
     $manager->update ( $todo );
     return [ 1007, [ ] ];
 } ) ) ) );
